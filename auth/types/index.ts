fd424465641fcde6fba39a96cc4e416c91b1d7cb
Dipak Khade <dipakhade214@gmail.com>
2025-12-30 15:36:39 +0530

    added zod types

---
+++ auth/types/index.ts
@@ -0,0 +1,14 @@
+import z, { email } from 'zod'
+
+export const signin_schema = z.object({
+    email: z.email(),
+    name: z.string(),
+    password: z.string().min(4),
+    role: z.enum(["student", "teacher"])
+})
+
+
+export const login_schema = z.object({
+    email: z.string(),
+    password: z.string().min(4)
+})
