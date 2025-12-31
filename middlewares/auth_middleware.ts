3ffd09def37287c9fe71c37e053e5e8a36929c28
Dipak Khade <dipakhade214@gmail.com>
2025-12-31 20:18:27 +0530

    added auth midd

---
+++ middlewares/auth_middleware.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from "express";
+import { APIResponse } from "../classes/responses";
+import jwt from 'jsonwebtoken';
+import { JWT_SEC } from "../config";
+
+export const auth_moddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.token as string;
+
+    if(!token) {
+        res.json(APIResponse.error(`token not found`));
+        return;
+    }
+
+    const is_valid_token = jwt.verify(token, JWT_SEC) as {userId: string, role: string}
+
+    if(!is_valid_token) {
+        res.json(APIResponse.error(`invalid token!`));
+        return;
+    }
+    console.log('is_valid_token---', is_valid_token)
+
+    req.user_id = is_valid_token.userId
+
+    next();
+}
