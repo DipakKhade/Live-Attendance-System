3ec2cf9e84b1cad3ce78d02848a0327759e01f06
Dipak Khade <dipakhade214@gmail.com>
2026-01-01 14:50:47 +0530

    added socket port

--- config.ts
+++ config.ts
@@ -6,3 +6,5 @@ export const DB_URL = process.env.MONOGODB_URL ?? ''
 export const PASSWORD_HASH_SALT_ROUNDS = 10;
 
 export const JWT_SEC = process.env.JWT_SEC ?? 'asd'
+
+export const WEBSOCKET_SERVER_PORT = 3009;
