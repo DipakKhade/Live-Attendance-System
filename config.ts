dcc63f28f051f861c659fee87dbd6bff6eac0b93
Dipak Khade <dipakhade214@gmail.com>
2026-01-01 22:18:58 +0530

    made single config obj

--- config.ts
+++ config.ts
@@ -1,10 +1,7 @@
 
-export const SERVER_PORT = 3008;
-
-export const DB_URL = process.env.MONOGODB_URL ?? ''
-
-export const PASSWORD_HASH_SALT_ROUNDS = 10;
-
-export const JWT_SEC = process.env.JWT_SEC ?? 'asd'
-
-export const WEBSOCKET_SERVER_PORT = 3009;
+export const SERVER_CONFIG = {
+    SERVER_PORT: 3008,
+    DB_URL: process.env.MONOGODB_URL ?? '',
+    PASSWORD_HASH_SALT_ROUNDS: 10,
+    JWT_SEC: process.env.JWT_SEC ?? 'asd'
+}
